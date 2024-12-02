c6999cb6c02ceddb2aa618020d717cd00f317f06
arunkumarvas <arunkumarvasg@gmail.com>
2024-12-02 20:00:57 +0530

    edited faculty page

--- src/Users/Faculty.js
+++ src/Users/Faculty.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Faculty() {
   return (
     <div>
-      <h1>Welcome to faculty page</h1>
+      <h1>Welcome to faculty </h1>
       <button><Link to ='/home'>Home</Link></button>
     </div>
   )
